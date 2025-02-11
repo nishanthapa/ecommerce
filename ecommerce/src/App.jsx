c7bb7655b47ac2760a43de8571c3a913fce27df7
Ashok-poudel <poudelashok77@gmail.com>
2025-02-11 15:51:28 +0545

    mula.

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,10 +1,18 @@
 import Header from "./components/Header/Header";
 import Hero from "./Components/Hero/Hero";
+import Compaines from "./components/Companies/Compaines";
+import Residences from "./components/Residences/Residences";
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Hero/>
+      <div>
+        <div className="white-gradient"> 
+          <Header/>
+        <Hero/>
+        </div>
+       </div>
+      <Compaines/>
+      <Residences/>
     </div>
   );
 }
---
+++ ecommerce/src/components/Residences/Residences.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Swiper, SwiperSlide,useSwiper} from 'swiper/react'
+import "swiper/css"
+import "./Residences.css"
+import data from '../../utils/slider.json'
+const Residences = () => {
+  return (
+   <section className='r-wrapper'>
+   <div className='paddings innerWidth r-container'>
+    div.r-head.flexColStart
+   </div>
+
+   </section>
+  )
+}
+
+export default Residences
