5160fda5d1de3f4ddc76737f1e9be7a9291b8992
Ashok-poudel <poudelashok77@gmail.com>
2025-02-11 14:13:17 +0545

    header

--- ecommerce/src/components/Header/Header.jsx
+++ ecommerce/src/components/Header/Header.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import './Header.css'
 const Header = () => {
   return (
-    <div>Header</div>
+ <section className='h-wrapper'>
+    <div className="flexCenter paddings innerWidth h-container">
+        <img src="./logo.png" alt="logo" width={100} />
+
+
+
+ <div className='flexCenter h-menu'>
+    <a href ="" >Residences</a>
+    <a href ="">Our value</a>
+    <a href ="">Contact Us</a>
+    <a href ="">Get Started</a>
+    <button className='button'>
+    <a href ="">Contact</a>
+    </button>
+   
+ </div>
+
+    </div>
+ </section>
   )
 }
 
